35eb235f4f4b2cf13507fbac3d0c6fe302ed618d
sir-udenna <udennaboy@gmail.com>
2021-04-08 03:30:54 -0500

    commit tree 900112b9decb8893bc035c5995c87ae16c3b82e8

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+// no longer has DOM node 'main#main'
+// has a 'newHeader' variable that points to node 'h1#victory'
+// has a 'newHeader' variable that points to node 'h1#victory'
+// has a 'newHeader' variable that points to node 'h1#victory' with "YOUR-NAME is the champion" inside
+
+document.getElementById('main').remove()
+let newHeader = document.createElement('h1')
+let newContent = document.createTextNode("Udenna is the champion");
+newHeader.setAttribute('id','victory');
+newHeader.appendChild(newContent);
